42de8bd061a9b97fc706788933861bada8358196
Akmal <isomadinovakmal1997@gmail.com>
2022-07-09 23:01:44 +0500

    added header and search icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import './App.css';
+import Search from './components/Search/Search';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App" />
+    <div className={styles.app}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>GitHub</h1>
+        <Search />
+      </header>
+    </div>
   );
 }
 
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './Search.module.css';
+import { ReactComponent as SearchIcon } from './SearchIcon.svg';
 
 function Search() {
   return (
     <div className={styles.inputWrapper}>
       <input className={styles.input} type="text" />
+      <SearchIcon className={styles.searchIcon} />
     </div>
   );
 }
